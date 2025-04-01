fdfac3f1b8f3edbda72ac4d4eed2f54f158986ab
Ziyad Hammad <ziyadhammad0@gmail.com>
2025-04-01 19:43:42 -0400

    interview session done

---
+++ app/(root)/interview/[id]/page.tsx
@@ -0,0 +1,42 @@
+import { redirect } from "next/navigation";
+import Image from "next/image";
+
+import { getRandomInterviewCover } from "@/lib/utils";
+import { getCurrentUser } from "@/lib/actions/auth.action";
+import { getInterviewById } from "@/lib/actions/general.action";
+import DisplayTechIcons from "@/components/DisplayTechIcons";
+import Agent from "@/components/Agent";
+
+const page = async ({ params }: RouteParams) => {
+  const { id } = await params;
+  const user = getCurrentUser()
+  const interview = await getInterviewById(id);
+
+  if (!interview) redirect("/");
+
+  return (
+    <>
+      <div className="flex flex-row gap-4 justify-between">
+        <div className="flex flex-row gap-4 items-center max-sm:flex-col">
+          <div className="flex flex-row gap-4">
+            <Image
+              className="rounded-full object-cover size-[40px]"
+              src={getRandomInterviewCover()}
+              height={40}
+              width={40}
+              alt="cover image"
+            />
+            <h3 className="capitalize" >{interview.role} Interview</h3>
+          </div>
+          <DisplayTechIcons techStack={interview.techstack} />
+        </div>
+        <p className="bg-dark-200 px-4 py-2 rounded-lg h-fit capitalize" >{interview.type}</p>
+      </div>
+
+      <Agent userName={user?.name} type={ user?.id} interviewId={id} type='interview' questions={interview.questions} />
+
+    </>
+  );
+};
+
+export default page;
--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { interviewer } from "@/constants";
 import { cn } from "@/lib/utils";
 import { vapi } from "@/lib/vapi.sdk";
 
@@ -19,11 +20,27 @@ interface SavedMessage {
   content: string;
 }
 
-const Agent = ({ userName, userId, type }: AgentProps) => {
+const Agent = ({ userName, userId, type, interviewId, questions }: AgentProps) => {
   const router = useRouter();
   const [isSpeaking, setIsSpeaking] = useState(false);
   const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
   const [messages, setMessages] = useState<SavedMessage[]>([]);
+
+  const handleGenerateFeedback = async (messages: SavedMessage[]) => {
+    console.log('generate feedback here.')
+
+    const { success, id } = {
+      success: true,
+      id: 'feedback-id'
+    }
+
+    if (success & id) {
+      router.push(`/interview/${interviewId}/feedback`)
+    } else {
+      console.log('error saving feedback')
+      router.push('/')
+    }
+  }
 
   useEffect(() => {
     const onCallStart = () => setCallStatus(CallStatus.ACTIVE);
@@ -60,18 +77,40 @@ const Agent = ({ userName, userId, type }: AgentProps) => {
   }, []);
 
   useEffect(() => {
-    if (callStatus === CallStatus.FINISHED) router.push("/");
+    if (callStatus === CallStatus.FINISHED) {
+      if (type === 'generate') {
+        router.push('/')
+      } else {
+        handleGenerateFeedback(messages)
+      }
+    }
+
   }, [messages, callStatus, type, userId]);
 
   const handleCall = async () => {
     setCallStatus(CallStatus.CONNECTING);
 
-    await vapi.start(process.env.NEXT_PUBLIC_VAPI_ASSISTANT_ID!, {
-      variableValues: {
-        username: userName,
-        userid: userId,
-      },
-    });
+    if (type === 'generate') {
+      await vapi.start(process.env.NEXT_PUBLIC_VAPI_ASSISTANT_ID!, {
+        variableValues: {
+          username: userName,
+          userid: userId,
+        },
+      });
+    } else {
+      let formattedQuestions = ''
+
+      if (questions) {
+        formattedQuestions = questions.map(question => `- ${question}`).join('\n')
+      }
+
+      await vapi.start(interviewer, {
+        variableValues: {
+          questions: formattedQuestions
+        }
+      })
+    }
+  
   };
 
   const handleDisconnect = async () => {
